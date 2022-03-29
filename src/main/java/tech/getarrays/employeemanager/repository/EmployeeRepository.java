e2ad9bc944ffc6357fff639839a41d5dc7c27de3
Yasmine Bouhaik <yasmine.bouhaik@gmail.com>
2022-03-29 22:11:25 +0200

    ok

---
+++ src/main/java/tech/getarrays/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package tech.getarrays.employeemanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.getarrays.employeemanager.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+	void deleteEmployeeById(Long id);
+	
+	 Employee findEmployeeById(Long id);
+	
+}
